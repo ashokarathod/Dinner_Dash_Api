ee8c7cfdf0523ac45bfca2aaccd0e99d73a59cc6
Ashok <exit>
2021-02-17 20:39:20 +0530

    curd operation for post api...

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -5,4 +5,44 @@ class Api::V1::PostsController < ApplicationController
     render json: @posts
   end
 
+  def show
+    @post = Post.find(params[:id])
+    render json: @post
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      render json: @post
+    else
+      render json: {error: "unable to create a post"}, status: 400
+    end
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post
+      @post.update(post_params)
+      render json: @post
+    else
+      render json: {error: "unable to update a post"}
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    if @post
+      @post.destroy(post_params)
+      render json: {message: "successfully deleted"}, status: 200
+    else
+      render json:{error: "unable to delete.."}, status: 400
+    end
+  end
+
+  private 
+
+  def post_params
+    params.permit(:post, :likes, :user_id)
+  end
+
 end
